3436aa14f95a8c6a63e051854d897bf8dcb1257e
Ge <648982006@qq.com>
2022-03-03 21:36:42 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Header} from './component/header'
 import {TopImage} from './component/top-image'
 import {MainPage} from './component/mainpage'
 
+
+
 function App() {
   return (
     <div className="App">
